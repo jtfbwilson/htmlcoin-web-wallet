5238541cb117907415bf5dbf76ebca2664a686e4
denuoweb <jaron.rosenau@gmail.com>
2018-05-09 03:02:34 -0700

    update qrcode html

--- src/controllers/RequestPayment.vue
+++ src/controllers/RequestPayment.vue
@@ -47,7 +47,7 @@ export default {
   },
   methods: {
     drawQrCode() {
-      qrcode.toDataURL(`qtum:${this.address}?amount=${this.amount}&message=${this.message}`, (err, url) => {
+      qrcode.toDataURL(`html:${this.address}?amount=${this.amount}&message=${this.message}`, (err, url) => {
         this.qr = url
       })
     }
