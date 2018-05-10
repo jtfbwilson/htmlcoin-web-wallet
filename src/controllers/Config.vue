527e71d337d809ae086738a747d233fe75071e19
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-05-10 05:47:24 -0700

    mainnet to livenet

--- src/controllers/Config.vue
+++ src/controllers/Config.vue
@@ -80,7 +80,7 @@ export default {
       network: loadConfig.network,
       networkSelect: [
         { value: 'testnet', text: this.$t('common.testnet')},
-        { value: 'mainnet', text: this.$t('common.mainnet')},
+        { value: 'livenet', text: this.$t('common.mainnet')},
       ],
       mode: loadConfig.mode,
       modeSelect: [
