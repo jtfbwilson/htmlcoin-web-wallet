5c6ba3770395d511fc1197f4bde7ed568cdc3127
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-05-13 18:10:53 -0700

    change background to gold

--- src/App.vue
+++ src/App.vue
@@ -145,7 +145,7 @@ export default {
       }
     },
     headerClass() {
-      return this.mode === 'normal' ? 'cyan' : 'orange'
+      return this.mode === 'normal' ? 'gold' : 'orange'
     }
   },
   components: {
